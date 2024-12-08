9934d15d685ba35ecb21d00f1a0b40105ea304f1
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2024-12-08 10:38:16 -0500

    load image from dataset

--- backend/tests/predict.py
+++ backend/tests/predict.py
@@ -3,7 +3,7 @@ from bookscanner_ai import BookPredictor
 def test_predict() -> None:
     book_predictor = BookPredictor()
     book_predictor.load_models()
-    result = book_predictor.predict("tests/img_1.jpg")
+    result = book_predictor.predict("../ai/dataset/images/img_1.jpg")
 
     if not result:
         print("No books detected.")
